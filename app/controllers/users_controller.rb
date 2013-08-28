355ed78169c28e088ca0dd5b71bc24afe1fb026b
tanias <tania.s@sigmainfo.net>
2013-08-28 12:12:36 +0530

    will paginate correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate :per_page => 10, :page => params[:page], 
+    @users = User.paginate :per_page => 10, :page => params[:page]
   end
 
   # GET /users/1
